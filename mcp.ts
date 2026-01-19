d4d6afff1518520e402aa11342efb54e64002faa
m4dd0c <manishsuthar078@gmail.com>
2026-01-19 06:20:12 +0530

    refactor: mcp.tools.ts -> mcp.ts and implementations moved to another file

--- mcp.ts
+++ mcp.ts
@@ -1,21 +1,12 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import * as z from "zod";
+import { WeatherInputSchema } from "./schemas.ts";
+import {
+  sayMyNameImplementation,
+  weatherImplementation,
+} from "./implementations";
 
 const mcp = new McpServer({ name: "test-mcp", version: "1.0.0" });
-
-const weatherApi = (c: string) => {
-  switch (c.toLowerCase()) {
-    case "jalore":
-      return "Sunny, 32°C";
-    case "new york":
-      return "Cloudy, 22°C";
-    case "london":
-      return "Rainy, 18°C";
-    default:
-      return "City not found";
-  }
-};
 
 mcp.registerTool(
   "Say My Name",
@@ -24,12 +15,11 @@ mcp.registerTool(
     description: "Returns the best fucking name.",
   },
   () => {
-    const text = "Not Heisenberg But m4dd0c";
     return {
       content: [
         {
           type: "text" as const,
-          text,
+          text: sayMyNameImplementation(),
         },
       ],
     };
@@ -41,21 +31,15 @@ mcp.registerTool(
   {
     title: "Get Weather Information",
     description: "Fetches weather information for a given location.",
-    inputSchema: z.object({
-      city: z
-        .string()
-        .describe("The name of the city to get weather information for.")
-        .min(1)
-        .max(100),
-    }),
+    inputSchema: WeatherInputSchema,
   },
-  (city) => {
-    const weather = weatherApi(city.city);
+  ({ city }) => {
+    const weather = weatherImplementation({ city });
     return {
       content: [
         {
           type: "text" as const,
-          text: `The weather in ${city.city} is: ${weather}`,
+          text: `The weather in ${city} is: ${weather}`,
         },
       ],
     };
