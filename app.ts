415b8eab139c9afb9fb9779969a39c8cd4c0661f
m4dd0c <manishsuthar078@gmail.com>
2026-01-19 07:18:49 +0530

    feat: call added

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 import { GEMINI_API_KEY } from "./constants.ts";
-import connectMCP from "./mcp.ts";
 import { seedDB } from "./rag.ts";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage } from "@langchain/core/messages";
+import getAgent from "./agent.ts";
 
 export const model = new ChatGoogleGenerativeAI({
   model: "gemini-1.5-flash",
@@ -9,7 +10,36 @@ export const model = new ChatGoogleGenerativeAI({
   apiKey: GEMINI_API_KEY,
 });
 
-// connecting to mcp server
-await connectMCP();
-// seeding the vector store
-await seedDB();
+const main = async () => {
+  try {
+    const agent = await getAgent();
+    if (agent) {
+      await seedDB();
+      console.log("✅ Agent ready!\n");
+
+      // Example queries
+      const queries = [
+        "How are you?", // General → Direct
+        "What's the remote work policy?", // Office → RAG
+        "What's the weather in London?", // Weather → Tool
+        "Say my name", // Identity → Tool
+      ];
+
+      for (const query of queries) {
+        console.log(`You: ${query}`);
+        const result = await agent.invoke({
+          messages: [new HumanMessage(query)],
+        });
+
+        const reply =
+          result?.messages?.[result?.messages?.length - 1]?.content ||
+          "No Response from LLM";
+        console.log(`Agent: ${reply}\n`);
+      }
+    }
+  } catch (error) {
+    console.error("Error in main execution:", error);
+  }
+};
+
+main();
