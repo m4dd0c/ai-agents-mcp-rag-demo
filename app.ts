6a9c9db03ccee3060ea71d5480baa1fa1e3c3b69
m4dd0c <manishsuthar078@gmail.com>
2026-01-19 06:19:34 +0530

    chore

--- app.ts
+++ app.ts
@@ -1,5 +1,13 @@
-import connectMCP from "./mcp.tools.ts";
+import { GEMINI_API_KEY } from "./constants.ts";
+import connectMCP from "./mcp.ts";
 import { seedDB } from "./rag.ts";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+export const model = new ChatGoogleGenerativeAI({
+  model: "gemini-1.5-flash",
+  temperature: 0.4, // less creative, more factual
+  apiKey: GEMINI_API_KEY,
+});
 
 // connecting to mcp server
 await connectMCP();
