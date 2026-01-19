b27f01e24a98ff76cf6e6b8d2950547ae954463b
m4dd0c <manishsuthar078@gmail.com>
2026-01-19 06:29:35 +0530

    refactor: tight code

--- implementations.ts
+++ implementations.ts
@@ -1,20 +1,14 @@
 import * as z from "zod";
 import type { WeatherInputSchema } from "./schemas";
 
-// functional implementation
 const sayMyNameImplementation = () => {
-  const text = "Not Heisenberg But m4dd0c";
-  return text;
+  return "Not Heisenberg But m4dd0c";
 };
 
 const weatherImplementation = ({
   city,
 }: z.infer<typeof WeatherInputSchema>) => {
-  return weatherApi(city);
-};
-
-const weatherApi = (c: string): string => {
-  switch (c.toLowerCase()) {
+  switch (city.toLowerCase()) {
     case "jalore":
       return "Sunny, 32°C";
     case "new york":
